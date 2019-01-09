21a76de91ae7c5bcb6a28de9a1c5036bc34ae400
Andrea Giammarchi <a.giammarchi@eyeo.com>
2019-01-09 21:14:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var file = process.argv.pop();
+
+fs.readFile(file, function (err, data) {
+  if (err) {
+    console.error('invalid file ' + file);
+    process.exit(1);
+  }
+  var content = data.toString('utf8')
+    .replace(/function _typeof\([^\1]+?(return\s+_typeof\([^}]+\})/g, '')
+    .replace(/(\s+)_typeof\(/g, '$1typeof(');
+  fs.writeFile(file, content, function (err) {
+    if (err) {
+      console.error('unable to write ' + file);
+      process.exit(1);
+    }
+  });
+});
